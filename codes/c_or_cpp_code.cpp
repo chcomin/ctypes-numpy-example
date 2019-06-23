8b12dd0390063324379041a884a881621d91c961
chcomin <abc@cdf.com>
2019-06-23 17:06:55 -0300

    Added print statements

--- codes/c_or_cpp_code.cpp
+++ codes/c_or_cpp_code.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+
+void print_values(int* arr_in, unsigned int* shape) {
+
+  unsigned int num_rows, num_cols, row, col;
+
+  num_rows = shape[0];
+  num_cols = shape[1];
+
+  std::cout <<  "C array values: " << std::endl;
+  for (row=0; row<num_rows; row++) {
+    for (col=0; col<num_cols; col++) {
+      std::cout << arr_in[row*num_cols + col] << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout <<  std::endl;
+}
+
 void apply_operation(int *arr_in, int *arr_out, unsigned int *shape) {
 
   unsigned int num_rows, num_cols, row, col;
@@ -18,6 +37,7 @@ extern "C" {
 #endif
 
 int operation(int *arr_in, int *arr_out, unsigned int *shape) {
+  print_values(arr_in, shape);
   apply_operation(arr_in, arr_out, shape);
   return 0;
 }
--- codes/reference.py
+++ codes/reference.py
@@ -2,25 +2,34 @@ import numpy as np
 import numpy.ctypeslib as npct
 import ctypes as ct
 
-# Importa modulo e define argumentos
-mymodule = npct.load_library('libmymodule', '.')
-args = [npct.ndpointer(dtype=np.int32, ndim=3, flags=['C_CONTIGUOUS']),
-        npct.ndpointer(dtype=np.int32, ndim=3, flags=['C_CONTIGUOUS']),
+# Import module
+mymodule = npct.load_library('libpymodule', '.')
+args = [npct.ndpointer(dtype=np.int32, ndim=2, flags=['C_CONTIGUOUS']),
+        npct.ndpointer(dtype=np.int32, ndim=2, flags=['C_CONTIGUOUS']),
         npct.ndpointer(dtype=np.uint32, ndim=1, flags=['C_CONTIGUOUS'])
     ]
 mymodule.operation.argtypes = args
 mymodule.operation.restype = ct.c_int
 
+N = 5
 
-vol_in = (10*np.random.randn(5, 4, 3)).astype(np.int32)
+arr_in = np.random.randint(0, 10, (N, N), dtype = np.int32)
 
-for plane in vol_in:
-    for row in plane:
-        for val in row:
-            print(val, end=' ')
-        print('')
-    print('')
+print("Numpy input array values: ")
+for row in arr_in:
+    for val in row:
+        print(val, end=' ')
+    print()
+print()
 
-vol_out = np.zeros_like(vol_in)
-shape = np.array(vol_in.shape, dtype=np.uint32)
-mymodule.operation(vol_in, vol_out, shape)
+
+arr_out = np.zeros_like(arr_in)
+shape = np.array(arr_in.shape, dtype=np.uint32)
+mymodule.operation(arr_in, arr_out, shape)
+
+print("Numpy output array values: ")
+for row in arr_out:
+    for val in row:
+        print(val, end=' ')
+    print()
+print()
